a16f2bb239f61d8a9322be9474e2512fcb046624
Razvan Timis <r@timis.dev>
2024-05-19 20:33:53 +0300

    Add shapes

---
+++ shapes/shapes.go
@@ -0,0 +1,26 @@
+package shapes
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return 3.141592653589793 * c.Radius * c.Radius
+}
